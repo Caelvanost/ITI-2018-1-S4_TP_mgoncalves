3592b4ea5a4db374d9d7def16d6d1b8b40322b4b
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2018-03-20 15:01:01 +0100

    Firsts tests with Vue

--- src/ITI.PrimarySchool.WebApp/App/primary-school/src/components/Home.vue
+++ src/ITI.PrimarySchool.WebApp/App/primary-school/src/components/Home.vue
@@ -1,6 +1,36 @@
 <template>
     <section class="jumbotron text-center">
         <div class="container">
+            {{ title }}
+
+            <button type="button" @click="changeTitle()">Change title</button>
+
+            <input type="text" v-model="title">
+
+            <div v-if="showAlert">
+                Alerte !
+            </div>
+
+            <button type="button" @click="showAlert = true">Afficher l'alerte</button>
+
+            <ul>
+                <li v-for="fruit of fruits">
+                    {{ fruit }}
+                </li>
+            </ul>
+
+            <div>
+                {{ counter }}
+                <button type="button" @click="counter += 1">Increment</button>
+                <div v-if="isLessThanTen">Le nombre est plus petit que 10</div>
+            </div>
+
+            <ul>
+                <li v-for="fruit of fruitsNotOrange">
+                    {{ fruit }}
+                </li>
+            </ul>
+
             <h1 class="jumbotron-heading">Bienvenue sur ITI.PrimarySchool</h1>
             <p class="lead text-muted">Cette application est réalisée à l'aide du framework Vue 2.</p>
             <p>
@@ -13,7 +43,35 @@
 
 <script>
 export default {
+    data() {
+        return {
+            title: 'Mon titre',
+            showAlert: false,
+            fruits: ['apple', 'orange', 'pear'],
+            counter: 0
+        }
+    },
 
+    methods: {
+        changeTitle() {
+            this.title = 'Mon nouveau titre'
+        },
+
+        resetTitle() {
+            this.title = '';
+            this.changeTitle();
+        }
+    },
+
+    computed: {
+        isLessThanTen() {
+            return this.counter < 10;
+        },
+
+        fruitsNotOrange() {
+            return this.fruits.filter(x => x != 'orange');
+        }
+    }
 }
 </script>
 
